625e1adca5cd7c4a9af91351403cdeae2d1bc75a
HayzaHyaga <wallacepereira656@gmail.com>
2025-01-16 10:54:25 -0300

    Atualização na transição, funcionando clique na imagem

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     const privacyLink = document.getElementById('privacy-link');
 
+    // Cria o overlay
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    overlay.innerHTML = '<img src="pink.jpeg" alt="Logo do Site">';
+    document.body.appendChild(overlay);
+
+    // Remove o overlay após a página carregar
+    setTimeout(function() {
+        overlay.classList.add('hide');
+        setTimeout(function() {
+            document.body.removeChild(overlay);
+        }, 1500); // Tempo da transição em milissegundos
+    }, 500); // Pequeno atraso para garantir que a transição funcione
+
     if (privacyLink) {
         privacyLink.addEventListener('click', function(event) {
             event.preventDefault();
 
-            // Cria o overlay
-            const overlay = document.createElement('div');
-            overlay.className = 'overlay';
-            overlay.innerHTML = '<img src="pink.jpeg" alt="Logo do Site">';
-            document.body.appendChild(overlay);
-
-            // Adiciona a classe para mostrar o overlay
-            setTimeout(function() {
-                overlay.classList.add('show');
-            }, 10); // Pequeno atraso para garantir que a transição funcione
+            // Adiciona a classe para mostrar o overlay instantaneamente
+            overlay.classList.remove('hide');
+            overlay.classList.add('show');
 
             // Redireciona após a transição
             setTimeout(function() {
                 window.location.href = event.target.href;
-            }, 500); // Tempo da transição em milissegundos
+            }, 10); // Tempo da transição em milissegundos
         });
     }
 });
